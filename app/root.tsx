d3a856dd772c71c5b7769556aa6b090e066df4f1
Nathan Strong <nathan.strong1@ibm.com>
2022-11-13 15:54:37 -0600

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "./styles/main.css"
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "NotePad 5000",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function links() {
+  return [
+    { rel: "stylesheet", href: styles },
+  ];
+}
